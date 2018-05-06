d03d48c1d8936227eea3d7f5979a06c0e18e89c9
adailtonribeiro <adailton.ribeiro1@gmail.com>
2018-05-06 16:24:17 -0300

    add commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,34 +1,10 @@
 module.exports = {
+  // full config on https://github.com/marionebl/commitlint/blob/master/@commitlint/config-conventional/index.js
+  // All rules on https://github.com/marionebl/commitlint/blob/master/docs/reference-rules.md
   rules: {
     'body-leading-blank': [1, 'always'],
     'footer-leading-blank': [1, 'always'],
     'header-max-length': [2, 'always', 72],
-    'scope-case': [2, 'always', 'lower-case'],
-    'subject-case': [
-      2,
-      'never',
-      ['sentence-case', 'start-case', 'pascal-case', 'upper-case']
-    ],
-    'subject-empty': [2, 'never'],
-    'subject-full-stop': [2, 'never', '.'],
-    'type-case': [2, 'always', 'lower-case'],
-    'type-empty': [2, 'never'],
-    'type-enum': [
-      2,
-      'always',
-      [
-        'build',
-        'chore',
-        'ci',
-        'docs',
-        'feat',
-        'fix',
-        'perf',
-        'refactor',
-        'revert',
-        'style',
-        'test'
-      ]
-    ]
+    'header-min-length': [2, 'always', 8]
   }
 };
